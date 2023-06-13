ed9fe1fdc3ab8a1555a93592a9c221da40b405c5
WtYt <83907568+Lulius3317@users.noreply.github.com>
2023-06-13 13:10:31 +0900

    wip select date

--- db.py
+++ db.py
@@ -5,14 +5,16 @@ DATABASE = 'SSS/db/database.db'
 def create_schedule_table():
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS schedule (schedule_id, schedule_name, user_id, year, month, day, start_time, end_time)")
-    cur.execute("SELECT schedule_id FROM schedule WHERE schedule_name = 'unused'")
+    cur.execute("CREATE TABLE IF NOT EXISTS schedule (schedule_id, schedule_name, user_id, year, month, date, start_time, end_time)")
+    cur.execute("SELECT schedule_id FROM schedule WHERE schedule_name = 'unused' AND year = " +str(year)+ " AND month = " +str(month)+ " AND date = " +str(date))
     data = cur.fetchall()
     if data == []:
-        cur.execute("INSERT INTO schedule VALUES(0,'unused',0,2023,6,1,0,41400)")
-        cur.execute("INSERT INTO schedule VALUES(1,'unused',0,2023,6,1,45000,81000)")
-        cur.execute("INSERT INTO schedule VALUES(2,'unused',1,2,2,2,2,2)")
-        cur.execute("INSERT INTO schedule VALUES(3,'unused',1,3,3,3,3,3)")
+        cur.execute("INSERT INTO schedule VALUES(0,'unused',0,2023,6,1,0,7200)")
+        cur.execute("INSERT INTO schedule VALUES(1,'unused',0,2023,6,1,54000,81000)")
+        cur.execute("INSERT INTO schedule VALUES(2,'unused',0,2023,6,1,18000,39600)")
+        cur.execute("INSERT INTO schedule VALUES(3,'unused',0,2023,6,1,14400,16200)")
+        cur.execute("INSERT INTO schedule VALUES(4,'unused',1,2023,6,1,0,1800)")
+        cur.execute("INSERT INTO schedule VALUES(5,'unused',1,2023,6,1,0,1800)")
         con.commit()
     con.close()
 
