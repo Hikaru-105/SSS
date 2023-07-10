d66cf26827c676150df72888db5ba32687e36d88
WtYt <83907568+Lulius3317@users.noreply.github.com>
2023-07-10 22:02:42 +0900

    html style added

--- main.py
+++ main.py
@@ -353,81 +353,79 @@ def Gcalendar(year,month):
     if day_upper_left == -6:
         day_upper_left=1
     month_last_day=calendar.monthrange(year,month)[1]
+    con=sqlite3.connect(DATABASE)
+    cur=con.cursor()
+    #user_id(最初から保持)でDBから条件を絞りgroup_idを取得
     group_id=current_user.group
+    #group_idでDBから条件を絞りグループ全員のuser_idを配列kへ取得
+    k=cur.execute('SELECT * FROM user WHERE "group"='+ str(group_id)).fetchall()
+    group_name=cur.execute('SELECT group_name FROM "group" WHERE "group_id"='+ str(group_id)).fetchone()[0]
     db_schedule=[]
-    group_name=""
+    #year,month,kよりDBからすべてのメンバーのある月のスケジュールを配列db_scheduleへ取得
+    for i in k:
+        K=i[0]
+        db_schedule.append(cur.execute('SELECT * FROM schedule WHERE year= '+ str(year)
+                                       +" AND month= "+ str(month)
+                                       +" AND user_id="+ str(K)).fetchall())
+    con.close()
+
+    #どの日付に予定が入っているかの判定
+    #すべての日付(配列cl)に予定が入っていない状態(0)とする。
     cl=[0]*32
     RT=[[]for _ in range(32)]
-    if group_id != 0:
-        con=sqlite3.connect(DATABASE)
-        cur=con.cursor()
-        #user_id(最初から保持)でDBから条件を絞りgroup_idを取得
-        #group_idでDBから条件を絞りグループ全員のuser_idを配列kへ取得
-        k=cur.execute('SELECT * FROM user WHERE "group"='+ str(group_id)).fetchall()
-        group_name=cur.execute('SELECT group_name FROM "group" WHERE "group_id"='+ str(group_id)).fetchone()[0]
-        #year,month,kよりDBからすべてのメンバーのある月のスケジュールを配列db_scheduleへ取得
-        for i in k:
-            K=i[0]
-            db_schedule.append(cur.execute('SELECT * FROM schedule WHERE year= '+ str(year)
-            +" AND month= "+ str(month)
-            +" AND user_id="+ str(K)).fetchall())
-            con.close()
-            #どの日付に予定が入っているかの判定
-            #すべての日付(配列cl)に予定が入っていない状態(0)とする。
 
-            for i in range(32):
-                #mへ30分ごとの一日のスケジュールのlistを格納
-                #list(o)の中身は
-                # 0b010000000000000000000000000000000000000000111100
-                # のように0(予定がない)と1(予定がある)が格納されている。
-                #上の例は00:30~01:00と21:00~23:00に予定があることを表す
-                o=list(format(Ctimeline(db_schedule, i), '#050b'))
-                #zはlistのひとつ前の文字を格納する。
-                #都合のため1を格納しておく
-                z="1"
-                ts=0
-                te=0
-                u=[""]
-                #48は24時間*2(30分毎なので)
-                for j in range(48):
-                    #clは予定がない時間帯のある日を記録するためのlist
-                    #もし予定がない時間帯があるならclに格納。何日に予定があるかを示す。
-                    if o[j+2]=="0":
-                        cl[i]=i
-                        #zはlistのひとつ前の文字を格納している
-                        #今の文字と前の文字が
-                        #0,0なら終わりの時間を30分伸ばす
-                        if o[j+2]=="0" and z=="0":
-                            te=te+30*60
-                            #0,1なら初めの時間を記録し、終わりの時間をその30分後とする。
-                        elif o[j+2]=="0" and z=="1":
-                            ts=30*60*j
-                            te=ts+30*60
-                            #1,0なら初めの時間と終わりの時間をsからhやmに直して文字列としてuに格納する。
-                        elif o[j+2]=="1" and z=="0":
-                            ah=int(ts/(60*60))
-                            am=int((ts%(60*60))
-                            /60)
-                            bh=int(te/(60*60))
-                            bm=int((te%(60*60))
-                            /60)
-                            u.append(f'{ah:02}'+":"+f'{am:02}'+"~"+f'{bh:02}'+":"+f'{bm:02}')
-                            #1,1の場合は何もしない
-                            #もしteが86400秒(24時間)になったら
-                            #初めの時間と終わりの時間をsからhやmに直して文字列としてuに格納する。
-                            if te==86400:
-                                ah=int(ts/(60*60))
-                                am=int((ts%(60*60))
-                                /60)
-                                bh=int(te/(60*60))
-                                bm=int((te%(60*60))
-                                /60)
-                                u.append(f'{ah:02}'+":"+f'{am:02}'+"~"+f'{bh:02}'+":"+f'{bm:02}')
-                                #zに今回の文字を格納して次回へ
-                                z=o[j+2]
-                                #1日の空いてる時間帯の格納されたlistを日付と同じindexの配列RTへ格納する
-                                RT[i]=u
-
+    for i in range(32):
+        #mへ30分ごとの一日のスケジュールのlistを格納
+        #list(o)の中身は
+        # 0b010000000000000000000000000000000000000000111100
+        # のように0(予定がない)と1(予定がある)が格納されている。
+        #上の例は00:30~01:00と21:00~23:00に予定があることを表す
+        o=list(format(Ctimeline(db_schedule, i), '#050b'))
+        #zはlistのひとつ前の文字を格納する。
+        #都合のため1を格納しておく
+        z="1"
+        ts=0
+        te=0
+        u=[""]
+        #48は24時間*2(30分毎なので)
+        for j in range(48):
+            #clは予定がない時間帯のある日を記録するためのlist
+            #もし予定がない時間帯があるならclに格納。何日に予定があるかを示す。
+            if o[j+2]=="0":
+                cl[i]=i
+            #zはlistのひとつ前の文字を格納している
+            #今の文字と前の文字が
+            #0,0なら終わりの時間を30分伸ばす
+            if o[j+2]=="0" and z=="0":
+                te=te+30*60
+            #0,1なら初めの時間を記録し、終わりの時間をその30分後とする。
+            elif o[j+2]=="0" and z=="1":
+                ts=30*60*j
+                te=ts+30*60
+            #1,0なら初めの時間と終わりの時間をsからhやmに直して文字列としてuに格納する。
+            elif o[j+2]=="1" and z=="0":
+                ah=int(ts/(60*60))
+                am=int((ts%(60*60))
+                       /60)
+                bh=int(te/(60*60))
+                bm=int((te%(60*60))
+                       /60)
+                u.append(f'{ah:02}'+":"+f'{am:02}'+"~"+f'{bh:02}'+":"+f'{bm:02}')
+            #1,1の場合は何もしない
+            #もしteが86400秒(24時間)になったら
+            #初めの時間と終わりの時間をsからhやmに直して文字列としてuに格納する。
+            if te==86400:
+                ah=int(ts/(60*60))
+                am=int((ts%(60*60))
+                       /60)
+                bh=int(te/(60*60))
+                bm=int((te%(60*60))
+                       /60)
+                u.append(f'{ah:02}'+":"+f'{am:02}'+"~"+f'{bh:02}'+":"+f'{bm:02}')
+            #zに今回の文字を格納して次回へ
+            z=o[j+2]
+        #1日の空いてる時間帯の格納されたlistを日付と同じindexの配列RTへ格納する
+        RT[i]=u
 
     return render_template('G_Schedule/frmGroupSchedule.html',
                            year=year,
